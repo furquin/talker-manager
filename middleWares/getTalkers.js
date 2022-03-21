6432281d4f041a1c0d9cce047ab60484bf3ea7a9
furquin <laert.ff@gmail.com>
2022-03-21 15:46:16 -0300

    cria função que lê arquivo especifico,  caso ok retorna um objeto caso erro log do erro

---
+++ middleWares/getTalkers.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+
+const getTalkers = async (_req, res) => {
+    try {
+        const fileContent = await fs.readFile('./talker.json', 'utf-8');
+        const fileParse = JSON.parse(fileContent);
+
+        return res.status(200).json(fileParse);
+    } catch (error) {
+        return res.status(200).json(console.log(error));
+    }
+};
+
+module.exports = { getTalkers };
