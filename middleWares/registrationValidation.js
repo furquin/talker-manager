f33f6ade8d3fe27152a8344da1561eadeb95782e
furquin <laert.ff@gmail.com>
2022-03-22 16:44:30 -0300

    implementa validação para formato de data e avaliação

--- middleWares/registrationValidation.js
+++ middleWares/registrationValidation.js
@@ -36,3 +36,22 @@ const ageValidation = (req, res, next) => {
 
     next();
 };
+
+const formatValidation = (req, res, next) => {
+    const { talk: { watchedAt, rate } } = req.body;
+    const validFormat = /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$/;
+    const parseIntRate = parseInt(rate, 10);
+    
+    if (parseIntRate < 1 || parseIntRate > 5) {
+        return res.status(400).json(
+            { message: 'O campo "rate" deve ser um inteiro de 1 à 5' },
+        );
+    }
+    if (validFormat.test(watchedAt) === false) {
+        return res.status(400).json(
+            { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' },
+        );
+    }
+
+    next();
+};
