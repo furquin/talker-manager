8bc542a27196c541c435a9e6c2ffdde0c5d1ecd3
furquin <laert.ff@gmail.com>
2022-03-22 20:30:12 -0300

    refatora código para corrigir erro de validação

--- middleWares/registrationValidation.js
+++ middleWares/registrationValidation.js
@@ -37,28 +37,43 @@ const ageValidation = (req, res, next) => {
     next();
 };
 
-const formatValidation = (req, res, next) => {
-    const { talk: { watchedAt, rate } } = req.body;
-    const validFormat = /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$/;
+const rateValidation = (req, res, next) => {
+    const { talk: { rate } } = req.body;
     const parseIntRate = parseInt(rate, 10);
-    
     if (parseIntRate < 1 || parseIntRate > 5) {
         return res.status(400).json(
             { message: 'O campo "rate" deve ser um inteiro de 1 à 5' },
         );
     }
+    if (!rate) {
+       return res.status(400).json(
+            { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
+        ); 
+    }
+    next();
+};
+
+const watchedAtValidation = (req, res, next) => {
+    const { talk: { watchedAt } } = req.body;
+    const validFormat = /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$/;
+
+    if (!watchedAt) {
+       return res.status(400).json(
+            { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
+        ); 
+    }
+
     if (validFormat.test(watchedAt) === false) {
         return res.status(400).json(
             { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' },
         );
     }
-
     next();
 };
 
 const talkerValidation = (req, res, next) => {
     const { talk } = req.body;
-    if (!talk || !talk.rate || !talk.watchedAt) {
+    if (!talk) {
         return res.status(400).json(
             { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
         );
@@ -71,5 +86,6 @@ module.exports = {
     nameValidation,
     ageValidation,
     talkerValidation,
-    formatValidation,
+    rateValidation,
+    watchedAtValidation,
 };
