2a3c16be19379017ee56da9457eba646f16617cb
furquin <laert.ff@gmail.com>
2022-03-22 16:45:15 -0300

    implementa validação de campos obrigatórios

--- middleWares/registrationValidation.js
+++ middleWares/registrationValidation.js
@@ -55,3 +55,21 @@ const formatValidation = (req, res, next) => {
 
     next();
 };
+
+const talkerValidation = (req, res, next) => {
+    const { talk } = req.body;
+    if (!talk || !talk.rate || !talk.watchedAt) {
+        return res.status(400).json(
+            { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
+        );
+    }
+    next();
+};
+
+module.exports = {
+    tokenValidation,
+    nameValidation,
+    ageValidation,
+    talkerValidation,
+    formatValidation,
+};
